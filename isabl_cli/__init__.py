3b204c24c4d426f147fd728854506042d46af7ec
Juan Medina <juansantiago.medina@gmail.com>
2019-03-04 18:54:31 -0500

    🚀 add more functions to init

--- isabl_cli/__init__.py
+++ isabl_cli/__init__.py
@@ -4,6 +4,16 @@ from os.path import abspath
 from os.path import dirname
 from os.path import join
 
+from isabl_cli.api import create_instance
+from isabl_cli.api import get_analyses
+from isabl_cli.api import get_experiments
+from isabl_cli.api import get_instance
+from isabl_cli.api import get_instances
+from isabl_cli.api import get_instances_count
+from isabl_cli.api import get_projects
+from isabl_cli.api import get_tree
+from isabl_cli.api import get_trees
+from isabl_cli.api import patch_instance
 from isabl_cli.app import AbstractApplication
 from isabl_cli.exceptions import MissingDataError
 from isabl_cli.exceptions import ValidationError
