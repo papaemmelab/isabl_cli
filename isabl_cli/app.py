60b8616849750ba7d88ef1aa5786b3ef54c0b1a2
Juan Esteban Arango <juanes.ao@gmail.com>
2020-01-20 18:55:07 -0500

    📊 add track event for Ran apps

--- isabl_cli/app.py
+++ isabl_cli/app.py
@@ -15,6 +15,7 @@ import traceback
 from cached_property import cached_property
 from click import progressbar
 from slugify import slugify
+import analytics
 import click
 
 from isabl_cli import api
@@ -945,6 +946,16 @@ class AbstractApplication:  # pylint: disable=too-many-public-methods
                 except self.skip_exceptions as error:  # pragma: no cover
                     skipped_tuples.append((i, error))
 
+        # track user app usage from cli
+        self.send_analytics(
+            command_tuples=command_tuples,
+            skipped_tuples=skipped_tuples,
+            commit=commit,
+            force=force,
+            restart=restart,
+            submitter=submit_analyses.__name__,
+        )
+
         if commit:
             click.echo(f"Running analyses with {submit_analyses.__name__}...")
             run_tuples = submit_analyses(self, command_tuples)
@@ -952,6 +963,49 @@ class AbstractApplication:  # pylint: disable=too-many-public-methods
             run_tuples = [(i, self._staged_message) for i, _ in command_tuples]
 
         return run_tuples, skipped_tuples
+
+    def send_analytics(
+        self, command_tuples, skipped_tuples, commit, force, restart, submitter
+    ):
+        """Send analytics event of analyses ran from cli."""
+        analyses_tuples = []
+        for i, _ in command_tuples:
+            status = (
+                "FORCED"
+                if force
+                else "RESTARTED"
+                if restart
+                else "SUBMITTED"
+                if commit
+                else self._staged_message
+            )
+            analyses_tuples.append((i, status))
+        for i, _ in skipped_tuples:
+            analyses_tuples.append((i, "INVALID"))
+
+        analyses = []
+        for i, status in analyses_tuples:
+            analysis = {
+                "analysis": i.pk,
+                "application": {
+                    "name": i.application.name,
+                    "pk": i.application.pk,
+                    "version": i.application.version,
+                },
+                "status": status,
+            }
+            analyses.append(analysis)
+
+        analytics.track(
+            system_settings.api_username,
+            "Ran app",
+            {
+                "analyses": analyses,
+                "submitter": submitter,
+                "valid": len(command_tuples),
+                "invalid": len(skipped_tuples),
+            },
+        )
 
     def get_command_script_path(self, analysis):
         """Get path to analysis command script."""
