878bc5cee8a8af340d2948d17f6a80a43659b5e6
Juan Esteban Arango <juanes.ao@gmail.com>
2020-07-21 16:32:15 -0400

    🐛 rollback httperror remove from signal

--- isabl_cli/commands.py
+++ isabl_cli/commands.py
@@ -3,6 +3,7 @@
 from collections import OrderedDict
 from glob import glob
 from os.path import join
+from requests.exceptions import HTTPError
 import json
 import os
 import shutil
@@ -372,9 +373,9 @@ def rerun_signals(filters):
         "signals", pk__gt=0, data__failure_traceback__isnull=False, **filters
     ):
         click.secho(f"Rerunning signal: {i.slug}", fg="yellow")
-        instance = api.get_instance(i.target_endpoint, i.target_id)
 
         try:
+            instance = api.get_instance(i.target_endpoint, i.target_id)
             api._run_signals(
                 endpoint=i.target_endpoint,
                 instance=instance,
