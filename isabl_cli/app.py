f57a2406398bb48fe6f27aa873886ac0cc7a198a
Zhou <zhouyangyu@hotmail.com>
2020-08-03 11:44:04 -0400

    🔧 refactor validate_individuals

--- isabl_cli/app.py
+++ isabl_cli/app.py
@@ -1705,19 +1705,34 @@ class AbstractApplication:  # pylint: disable=too-many-public-methods
             assert i.sample.category == "NORMAL", msg
 
     def validate_individuals(self, targets, references):
-        """Validate pairs are of the same individual."""
-        for target, reference in zip(targets, references):
-            tind = target["sample"]["individual"]["pk"]
-            rind = reference["sample"]["individual"]["pk"]
+        """
+        Validate pairs are of the same individual if the pipeline is matched;
+        Validate pairs are of the different individuals if the pipeline is unmatched.
+        """
+        if references:
+            targets_set = list(
+                {v["sample"]["individual"]["pk"]:v for v in targets}.values()
+            )
+            references_set = list(
+                {v["sample"]["individual"]["pk"]:v for v in references}.values()
+            )
+
+            assert len(targets_set) == 1, "One unique target individual is supported."
+            assert len(references_set) == 1, "One unique reference individual is supported."
+
+            tind = targets_set[0]["sample"]["individual"]
+            rind = references_set[0]["sample"]["individual"]
 
             if hasattr(self, "IS_UNMATCHED") and self.IS_UNMATCHED:
-                assert tind != rind, (
+                assert tind["pk"] != rind["pk"], (
                     "Different individuals required: "
-                    f"{target} and {reference} are of the same individual."
+                    f"{tind['system_id']} and {rind['system_id']} "
+                    "are of the same individual."
                 )
             else: # application is designed for matched analyses
-                assert tind == rind, (
+                assert tind["pk"] == rind["pk"], (
                     "Same individual required: " 
-                    f"{target} and {reference} are of different individuals."
+                    f"{tind['system_id']} and {rind['system_id']} "
+                    "are of different individuals."
                 )
 
