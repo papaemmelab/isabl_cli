a008e8d918ef84aee2802a18add739edc2a11cc3
Juan E. Arango <juanes.ao@gmail.com>
2022-11-04 16:03:39 -0400

    ✅ try again with GH_TOKEN auth

--- tests/test-with-compose.sh
+++ tests/test-with-compose.sh
@@ -16,7 +16,7 @@ ISABL_BRANCH=${TRAVIS_PULL_REQUEST_BRANCH:-${TRAVIS_BRANCH:-master}}
 echo "ISABL_BRANCH set to $ISABL_BRANCH given travis branch: $TRAVIS_BRANCH $TRAVIS_PULL_REQUEST_BRANCH"
 
 # clone api from github
-rm -rf $API_DIR && git clone https://github.com/papaemmelab/isabl_api.git $API_DIR
+rm -rf $API_DIR && git clone https://$GH_TOKEN@github.com/papaemmelab/isabl_api.git $API_DIR
 cd $API_DIR && (git checkout $ISABL_BRANCH || true) && docker-compose build && docker-compose up -d
 
 # give some time to API to start and test Isabl CLI
