ba3e62eb6637276db6034c0f687c9c1b1199d7f0
Juan E. Arango <juanes.ao@gmail.com>
2025-08-01 08:09:28 -0400

    🔧 fix hardcoded data return

--- isabl_cli/data.py
+++ isabl_cli/data.py
@@ -59,8 +59,7 @@ def raw_data_inspector(path):
                 return f"FASTQ_{fq_type}{i}"
 
     if re.search(r"\.f(ast)?q(\.gz)?$", path):
-        # raise click.UsageError(f"cant determine fastq type from: {path}")
-        return "FASTQ_R1"
+        raise click.UsageError(f"cant determine fastq type from: {path}")
 
     return None
 
